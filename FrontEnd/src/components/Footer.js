ab1838c222cce30eed412ff03a0066d26425ddad
SLRonzoni <slronzoni@gmail.com>
2023-02-11 12:28:27 -0300

    modified footer and home links target

--- FrontEnd/src/components/Footer.js
+++ FrontEnd/src/components/Footer.js
@@ -9,7 +9,7 @@ function Footer () {
     return (
         <footer> 
             <div className={ViewRegularUserOptions()}> 
-                <a href="https://www.google.com/maps/place/Av.+Dr.+Ricardo+Balb%C3%ADn+4780,+Buenos+Aires/@-34.5529207,-58.5005272,17z/data=!3m1!4b1!4m5!3m4!1s0x95bcb6e7c5d0b5a3:0xee2c4abafd3b0441!8m2!3d-34.5529251!4d-58.4983385" >
+                <a href="https://www.google.com/maps/place/Av.+Dr.+Ricardo+Balb%C3%ADn+4780,+Buenos+Aires/@-34.5529207,-58.5005272,17z/data=!3m1!4b1!4m5!3m4!1s0x95bcb6e7c5d0b5a3:0xee2c4abafd3b0441!8m2!3d-34.5529251!4d-58.4983385" target="_blank" rel="noreferrer noopener">
                     <FaIcons.FaMapMarkedAlt className="iconFooter"/> 
                 </a>
                 <span className="me-3">Av. R.Balbin 4780 - CABA</span>
@@ -21,11 +21,11 @@ function Footer () {
                     <FaIcons.FaMailBulk className="iconFooter me-2"/>
                 </a>      
                
-                <a href="http://instagram.com/somosmasong/" >
+                <a href="http://instagram.com/somosmasong/" target="_blank" rel="noreferrer noopener">
                    <FaIcons.FaInstagram className="iconFooter me-2"/> 
                 </a>
 
-                <a href="https://www.facebook.com/profile.php?id=100086643616310" >
+                <a href="https://www.facebook.com/profile.php?id=100086643616310" target="_blank" rel="noreferrer noopener">
                     <FaIcons.FaFacebook className="iconFooter me-2"/>
                 </a> 
             </div>                          
--- FrontEnd/src/components/Home.js
+++ FrontEnd/src/components/Home.js
@@ -31,7 +31,7 @@ function Home() {
 
         <div className="containerAddress">
           <div className="flex-Center">
-            <a href="https://www.google.com/maps/place/Av.+Dr.+Ricardo+Balb%C3%ADn+4780,+Buenos+Aires/@-34.5529207,-58.5005272,17z/data=!3m1!4b1!4m5!3m4!1s0x95bcb6e7c5d0b5a3:0xee2c4abafd3b0441!8m2!3d-34.5529251!4d-58.4983385" >
+            <a href="https://www.google.com/maps/place/Av.+Dr.+Ricardo+Balb%C3%ADn+4780,+Buenos+Aires/@-34.5529207,-58.5005272,17z/data=!3m1!4b1!4m5!3m4!1s0x95bcb6e7c5d0b5a3:0xee2c4abafd3b0441!8m2!3d-34.5529251!4d-58.4983385" target="_blank" rel="noreferrer noopener">
               <FaIcons.FaMapMarkedAlt className="iconHomeMap"/> 
             </a>
             <span className="me-3">Av. R.Balbin 4780 - CABA</span>
@@ -48,11 +48,11 @@ function Home() {
             <FaIcons.FaMailBulk className="iconHome me-4"/>
           </a>              
         
-          <a href="http://instagram.com/somosmasong/" >
+          <a href="http://instagram.com/somosmasong/" target="_blank" rel="noreferrer noopener">
             <FaIcons.FaInstagram className="iconHome me-4"/> 
           </a>
 
-          <a href="https://www.facebook.com/profile.php?id=100086643616310"  >
+          <a href="https://www.facebook.com/profile.php?id=100086643616310"  target="_blank" rel="noreferrer noopener">
             <FaIcons.FaFacebook className="iconHome me-4"/>
           </a>
         </div>  
